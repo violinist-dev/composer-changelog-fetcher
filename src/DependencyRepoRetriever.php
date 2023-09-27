ac452f02bf354c8e8085bfe60d3f98d14db1083a
Eirik S. Morland <eirik@morland.no>
2023-09-27 12:45:36 +0200

    Make it possible and easier to retrieve dependency repos

--- src/DependencyRepoRetriever.php
+++ src/DependencyRepoRetriever.php
@@ -35,6 +35,7 @@ class DependencyRepoRetriever
         $clone_path = '/tmp/' . md5($data->name);
         $repo_path = $data->source->url;
         $repo_parsed = parse_uri($repo_path);
+        $repo_path_overridden = false;
         if (!empty($repo_parsed)) {
             switch ($repo_parsed['_protocol']) {
                 case 'git@github.com':
@@ -43,7 +44,37 @@ class DependencyRepoRetriever
                         $this->authToken,
                         $repo_parsed['path']
                     );
+                    $repo_path_overridden = true;
                     break;
+            }
+            if (!$repo_path_overridden) {
+                switch ($repo_parsed["host"]) {
+                    case 'www.github.com':
+                    case 'github.com':
+                        $repo_path = sprintf('https://%s:x-oauth-basic@github.com/%s', $this->authToken, $repo_parsed["path"]);
+                        break;
+
+                    case 'www.gitlab.com':
+                    case 'gitlab.com':
+                        $repo_path = sprintf('https://oauth2:%s@gitlab.com/%s', $this->authToken, $repo_parsed["path"]);
+                        break;
+
+                    case 'www.bitbucket.org':
+                    case 'bitbucket.org':
+                    $repo_path = sprintf('https://x-token-auth:%s@bitbucket.org/%s', $this->authToken, $repo_parsed["path"]);
+                        break;
+
+                    default:
+                        $port = 443;
+                        if ($repo_parsed['scheme'] === 'http') {
+                            $port = 80;
+                        }
+                        if (!empty($repo_parsed["port"])) {
+                            $port = $repo_parsed["port"];
+                        }
+                        $repo_path = sprintf('%s://oauth2:%s@%s:%d/%s', $repo_parsed["scheme"], $this->authToken, $repo_parsed["host"], $port, $repo_parsed["path"]);
+                        break;
+                }
             }
         }
         if (!file_exists($clone_path)) {
